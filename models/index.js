e6ff43ba610d1bce44a2fffc288817195cd60e70
[carlos martinez] <theycallhellp@gmail.com>
2022-11-06 15:41:46 -0800

    Establish relationships between the models

--- models/index.js
+++ models/index.js
@@ -1,5 +1,34 @@
 //require models
+const User = require('./User');
+const Blog = require('./Blog');
+const Comment = require('./Comment');
 
 //establish relationship between models
 
+//User-Blog relationship
+User.hasMany(Blog, {
+    foreignKey: 'user_id',
+});
+
+Blog.belongsTo(User);
+
+//User-Comment relationship
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+});
+
+Comment.belongsTo(User);
+
+//Blog-Comment relationship
+Blog.hasMany(Comment, {
+    foreignKey: 'blog_id',
+});
+
+Comment.belongsTo(Blog);
+
 //export models to be used in api and 
+module.exports = {
+    User,
+    Blog,
+    Comment
+}
